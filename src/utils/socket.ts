cdbda49074ad581d2888b5abfb79040bec5ab495
Nkwo Chukwuemeka <emekankwo49@gmail.com>
2024-07-16 12:26:48 +0100

    updated

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -11,7 +11,7 @@ export const sessionId = nanoid();
 
 const socket = io(`${process.env.NEXT_PUBLIC_BASE_URL as string}`, {
   path: "/ws",
-  withCredentials: !!process.env.NEXT_PUBLIC_BASE_URL,
+  // withCredentials: !!process.env.NEXT_PUBLIC_BASE_URL,
 
   auth(cb) {
     cb({
