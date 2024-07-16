3e70185ca1dd4b333294479e701a8ef419b6e9b7
Nkwo Chukwuemeka <emekankwo49@gmail.com>
2024-07-16 12:20:50 +0100

    update

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,8 +1,22 @@
+import { nanoid } from "nanoid";
 import { io } from "socket.io-client";
 
+export const sessionId = nanoid();
+
+// export const useRemoteState = create<IRemoteState>()(() => ({
+//   socket: createSocket(),
+//   room: null, // current room user is in, null maps to landing page
+//   connections: [],
+// }));
+
 const socket = io(`${process.env.NEXT_PUBLIC_BASE_URL as string}`, {
-  path: "/ws",
-  withCredentials: true,
+  withCredentials: !!process.env.NEXT_PUBLIC_BASE_URL,
+
+  auth(cb) {
+    cb({
+      sessionId,
+    });
+  },
 });
 
 const socketConected = () => {
